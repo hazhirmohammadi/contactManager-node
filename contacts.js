6d36a7bff4f067cef5dc437d4cde21c84ce1ea5f
hazhirmohammadi <hazhir6690@gmail.com>
2024-01-17 12:16:15 +0330

    feat: saved contacts in json file

--- contacts.js
+++ contacts.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
 const chalk = require("chalk");
 
-const addContact = (fullname, phone, email) => {
+const addContact = (args) => {
    const contacts = loadContacts();
-   contacts.push({fullname, phone, email});
-   console.log(chalk.green("contact Saved"));
+   const duplicateContact = contacts.find(c => c.fullname === args.fullname);
+   if (!duplicateContact) {
+      contacts.push({fullname:args.fullname, phone:args.phone, email:args.email});
+      saveContact(contacts)
+      console.log(chalk.green("contact Saved"));
+   }else{
+      console.log(chalk.red("contact already exist"));
+   }
+
 }
 
 const loadContacts = () => {
@@ -17,6 +24,11 @@ const loadContacts = () => {
       return [];
    }
 }
-module.exports={
+
+const saveContact = contacts => {
+  const data=JSON.stringify(contacts)
+   fs.writeFileSync('contacts.json',data);
+}
+module.exports = {
    addContact,
 }
--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const yargs = require("yargs");
+const {addContact} = require("./contacts");
+const chalk = require("chalk");
 
 yargs.command({
    command: "create",
-   aliases:["c","ct"],
+   aliases: ["c", "ct"],
    describe: "[Create new contact]",
    builder: {
       fullname: {
@@ -11,23 +13,35 @@ yargs.command({
          demandOption: true,
          type: "string"
       },
-      phone:{
-         alias:"p",
+      phone: {
+         alias: "p",
          describe: "person phone Number",
          demandOption: true,
          type: "number"
       },
-      email:{
-         alias:"e",
+      email: {
+         alias: "e",
          describe: "person phone email",
          demandOption: true,
          type: "string"
       }
    },
-   handler(args){
-
-      console.log(args.fullname,args.email,args.phone)
+   handler(args) {
+      addContact({
+         fullname: args.fullname,
+         phone: args.phone,
+         email: args.email
+      })
    }
 })
+yargs.command({
+   command: "list",
+   aliases: ["l"],
+   describe: `${chalk.yellow("[listing ths save contacts]")}`,
+   handler() {
+
+   }
+})
+
 yargs.parse()
 // console.log(yargs.argv)
